2328b357c522c2d16bd84c7cee65b648a0ec4f41
Yasin Özen <yasinozen35@gmail.com>
2022-02-20 00:41:31 +0300

    İmage Üretimi başladı!

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/public', express.static('public'))
 const Instagram = require('instagram-web-api');
 const FileCookieStore = require("tough-cookie-filestore2");
 const cron = require("node-cron");
 require('dotenv').config();
 const port = process.env.PORT || 4000;
+app.set('view engine', 'pug')
 //00 00 * * *
 //40 14 14 2 *
 
 const Data = require('./data.js');
-/*
-let proje = new Data();
 
-proje.setSubject('ayet');
-//parametre olarak text size gönder
-const text = "Bakmakla hükümlü olduğu kişileri ihmal etmesi, kişiye günah olarak yeter! test";
-const info = "EbüDavut,Zekat,4-5";
-proje.setPhotoText(text, info);
-*/
+let proje = new Data();
+proje.generatePicture('hadis').then(()=>{
+    //console.log(proje.content);
+});
+
+app.get("/add", (req, res)=>{
+    const {subject, image} = req.query;
+    
+    if(subject){
+        proje.setSubject(subject);
+        proje.readFile().then(()=>{
+            if(image){
+                proje.generatePicture()
+            }
+            res.render('index', { title: 'Hey', message: 'Lütfen inputları doldurunuz!', image:proje.imageOut, fileArray:proje.lists.sort((a, b)=> b.created_date - a.created_date) })
+        });
+    }else{
+        res.render('index', { title: 'Hey', message: 'Lütfen inputları doldurunuz!', fileArray:[]})
+    }
+    
+});
+
+app.post("/add", async (req, res)=>{
+    const {content, source, subject} = req.body;
+    let data = {};
+    data['content'] = content.trim();
+    data['source']  = source.trim();
+    proje.addContentFromJson(data, subject).then(async (result)=>{
+        if(result.className=='success'){
+            await proje.readFile();
+            console.log(proje.lists.at(-1));
+        }
+        
+        res.render('index', {...result, fileArray:proje.lists.sort((a, b)=> b.created_date - a.created_date)});
+    });
+});
+
 cron.schedule("20 12 14 2 *", ()=>{
 
     const { INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD } = process.env
@@ -72,6 +106,8 @@ app.get('/', function (req, res) {
     console.log(req.query)
     res.send('hello world')
 })
+
+
 
 app.listen(port, ()=>{
    console.log(`Listening on port ${port}...`);
--- data.js
+++ data.js
@@ -3,29 +3,40 @@ const cheerio = require("cheerio");
 const fs = require('fs');
 const jsdom = require("jsdom");
 const Jimp = require('jimp');
+const stringSimilarity = require("string-similarity");
+const moment = require('moment');
+moment.locale('tr');
+
 class Data {
     constructor(){
         this.linkler=[];
         this.link="";
-        this.fileName="";
+        this.textFileName="";
         this.subject="";
         this.lists=[];
         this.ayet="";
         this.hadis="";
         this.dua="";
         this.$=null;
+        this.imageSrc="";
+        this.imageOut="";
+        this.content="";
+        this.source="";
     }
 
     setSubject(subject) {
-        this.setLink(subject);
-        this.setFileName(subject);
+        this.subject = subject;
+        if(this.subject == "") return
+        this.setLink();
+        this.setTextFileName();
+        this.setImageFileName();
     }
 
-    setLink(subject){
+    setLink(){
         let params = "";
-        subject.toLowerCase() == 'ayet' ? params = "Tagut-ile-ilgili-Ayetler" :
-        subject.toLowerCase() == 'hadis' ? params = "Ebu-Cendel-ve-Ebu-Basir" :
-        subject.toLowerCase() == 'dua' ? params = "Borcun-odenmesi-icin-Yapilan-Dualar" : "";
+        this.subject.toLowerCase() == 'ayet' ? params = "Tagut-ile-ilgili-Ayetler" :
+        this.subject.toLowerCase() == 'hadis' ? params = "Ebu-Cendel-ve-Ebu-Basir" :
+        this.subject.toLowerCase() == 'dua' ? params = "Borcun-odenmesi-icin-Yapilan-Dualar" : "";
         this.link = `https://sahihhadisler.com/konu/detay/${params}`;
     }
 
@@ -42,25 +53,17 @@ class Data {
         this.setLists()
     }
 
-    setFileName(name){
-        name == 'ayet' ?  this.fileName = 'ayetler.json' :
-        name == 'hadis' ?  this.fileName = 'hadisler.json' :
-        name == 'dua' ?  this.fileName = 'dualar.json' : '';
+    setTextFileName(){
+        this.subject == 'ayet' ?  this.textFileName = 'ayetler.json' :
+        this.subject == 'hadis' ?  this.textFileName = 'hadisler.json' :
+        this.subject == 'dua' ?  this.textFileName = 'dualar.json' : '';
     }
 
-    readJsonFromFile(){
-        let json = {};
-        fs.readFile(this.fileName, (err, data) => {
-            if (!err){
-                json = JSON.parse(data);
-            }
-        });
-        return json;
-    }
-
-    exportToJsonFile(jsonData) {
-        const data = Object.assign(this.readJsonFromFile(), jsonData);
-        fs.writeFileSync(this.fileName, JSON.stringify(data));
+    setImageFileName(){
+        if(this.subject == 'ayet' || this.subject == 'hadis' || this.subject == 'dua'){
+            this.imageSrc=`./assets/images/source/blank_${this.subject}.jpg`;
+            this.imageOut=`./public/images/${this.subject}.jpg`;
+        }
     }
 
     async setLists(){
@@ -78,6 +81,7 @@ class Data {
             })*/
         });
     }
+
     stringToBytes(text) {
         const length = text.length;
         const result = new Uint8Array(length);
@@ -89,29 +93,163 @@ class Data {
         return result;
     }
     
-    async setPhotoText(text, info){
-        const image = await Jimp.read("./assets/images/source/blank_hadis.jpg");
-        
-        const hadisText = await Jimp.loadFont('assets/fonts/hadis/text/50/font.fnt')
-        const hadisInfo = await Jimp.loadFont('assets/fonts/hadis/info/32/font.fnt')
-        
-        image.print(hadisText, 120, 350, {
-            text,
-            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
-            alignmentY: Jimp.VERTICAL_ALIGN_CENTER
-        }, 850, 700);
-
-        image.print(hadisInfo, 365, 770, {
-            text: info,
-            alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT,
-            alignmentY: Jimp.VERTICAL_ALIGN_CENTER
-        }, 850, 700);
-        //maxtext 37 font size 264 length
-        image.write("photoNew.jpg");
+    async generatePicture(subject){
+        this.setSubject(subject);
+        if(this.subject == "") return
+        await this.getText();
+        await this.photoOptions();
+        await this.addTextToPicture();
     }
 
-    setAyet(){
+    async getText(){
 
+        console.log(moment(new Date()).format("DD/MM/YYYY"));
+        console.log(moment("2022-02-19").format("LLL"))
+        console.log(moment("2022-02-19").format("X"))
+        console.log(moment("2022-02-19").unix())
+        console.log(moment.unix(moment("2022-02-19").unix()).format("LLL"))
+
+        let fileArray = await this.readFile();
+        fileArray.sort((a, b)=>{return b.created_date - a.created_date});
+        const findObj = fileArray.find((element) => element["isPublished"] == false);
+        let {content, source} = findObj;
+        this.content = content;
+        this.source = source;
+    }
+
+    readFile(){
+        return new Promise(resolve => {
+            fs.readFile(this.textFileName, (err, data) => {
+                let arrayList = [];
+                if (!err){
+                    if(data!=undefined){
+                        Object.values(JSON.parse(data)).forEach((value)=>{
+                            arrayList.push(value);
+                        })
+                    }
+                }
+                this.lists = arrayList;
+                resolve(arrayList);
+            });
+        });
+    }
+
+    textCompare(array, key, text){
+        let compareNumber = 0;
+        array.forEach((element)=>{
+            const comparePoint = stringSimilarity.compareTwoStrings(element[key], text);
+            if(comparePoint > 0.5) compareNumber++
+        })
+        return compareNumber
+    }
+
+    async addContentFromJson(jsonData, subject){
+        
+        if( jsonData.content == undefined || jsonData.source == undefined || subject == undefined) return {
+            className:"error",
+            message:"Lütfen boş geçmeyiniz!"
+        }
+        
+        this.setSubject(subject);
+        
+        let fileArray = await this.readFile();
+
+        if(this.textCompare(fileArray, "content", jsonData.content) == 0 && jsonData.content.length < 264){
+            jsonData['id'] = fileArray.length;
+            jsonData['created_date'] = moment().unix();
+            jsonData['isPublished'] = false;
+            jsonData['publish_date'] = null;
+
+            let jsonArray = [];
+            jsonArray.push(jsonData);
+
+            await this.saveFile(fileArray, jsonArray);
+            return {
+                className:"success",
+                message:"Başarılı"
+            }
+        }else{
+            return {
+                className:"error",
+                message:"Daha önce benzer metin eklenmiş!"
+            };
+        }
+    }
+
+    async saveFile(fileArray, jsonArray) {
+        return new Promise((resolve,reject) => {
+            try{
+                const data = fileArray.concat(jsonArray);
+                fs.writeFileSync(this.textFileName, JSON.stringify(data));
+                resolve(true);
+            }catch (err) {
+                reject(false);
+            }
+        })
+    }
+
+    async photoOptions(){
+        let contentFontSize = 0, sourceFontSize = 32;
+        let contentSize = this.content.length;
+
+        if(this.subject == 'ayet'){
+            if(contentSize >= 200 && contentSize <= 264){
+                contentFontSize = 37;
+            }else{
+                contentFontSize = 32;
+            }
+            sourceFontSize = 32;
+        }else if(this.subject == 'hadis'){
+            if(contentSize >= 160 && contentSize <= 199){
+                contentFontSize = 45;
+            }else if(contentSize >= 200 && contentSize <= 264){
+                contentFontSize = 37;
+            }else{
+                contentFontSize = 37;
+            }
+            sourceFontSize = 32;
+        }else{
+            if(contentSize >= 200 && contentSize <= 264){
+                contentFontSize = 32;
+            }else{
+                contentFontSize = 32;
+            }
+        }
+
+        let contentFontOptns = `assets/fonts/${this.subject}/content/${contentFontSize}/font.fnt`;
+        let sourceFontOptns = `assets/fonts/${this.subject}/source/${sourceFontSize}/font.fnt`;
+
+        const contentFont = await Jimp.loadFont(contentFontOptns);
+        const sourceFont = await Jimp.loadFont(sourceFontOptns);
+
+        return {
+            contentFont,
+            sourceFont
+        }
+    }
+
+    async addTextToPicture(){
+        const image = await Jimp.read(this.imageSrc);
+        const {contentFont, sourceFont} = await this.photoOptions();
+
+        if(this.content.length>0 && this.source.length>0){
+
+            image.print(contentFont, 120, 350, {
+                text: this.content,
+                alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+                alignmentY: Jimp.VERTICAL_ALIGN_CENTER
+            }, 850, 700);
+    
+            image.print(sourceFont, 365, 770, {
+                text: this.source,
+                alignmentX: Jimp.HORIZONTAL_ALIGN_LEFT,
+                alignmentY: Jimp.VERTICAL_ALIGN_CENTER
+            }, 850, 700);
+            //maxtext 37 font size 264 length
+            image.write(this.imageOut);
+        }else{
+            console.log("Content ve Source Belirlenmedi!")
+        }
     }
 };
 
